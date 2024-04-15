0ce93da30f4127d9571eb0805882be24f1331e43
Rainer Nsa <woldreamz@gmail.com>
2024-04-15 11:33:19 +0100

    City of God

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <link rel="icon" type="image/png" href="https://i.postimg.cc/xjwDWjgB/woldreamz-inc-favicon-black.png" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
       <Script src="https://kit.fontawesome.com/a788f2573e.js" crossOrigin="anonymous"></Script>
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
